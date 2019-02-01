7ee32839416b8fa9c238e263c6da8b7657d66ced
Laviousa <lalainanwar@hotmail.com>
2019-02-01 19:33:49 +0900

    Test

---
+++ app/src/main/java/com/example/zass/finalproject/quiz_Activity.java
@@ -0,0 +1,13 @@
+package com.example.zass.finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class quiz_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_);
+    }
+}
